cfd398de02f6ff0b2b6bac41a125fcfbd1418d5c
sugu2344 <suganesh234@gmail.com>
2023-12-12 16:36:14 +0530

    edit and delete  done

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,40 +1,34 @@
-import { Body, Controller, Get, Param, Post, Put,Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { studentsDTO } from './DTO/STUDENTS.DTO';
 import { Student } from './interface/student.interface';
 
 @Controller('student')
 export class StudentController {
-  personsService: any;
-  StudentService: any;
   constructor(private studentService: StudentService) {}
- 
+
   @Get("/")
   async getstudent(): Promise<Student[]> {
     return this.studentService.getStudents();
   }
-  
 
   @Post("/")
   async createStudent(@Body() data: studentsDTO): Promise<Student> {
-  const createdStudent = await this.studentService.createStudent(data);
-  console.log(createdStudent);
-  return createdStudent; 
-}
+    const createdStudent = await this.studentService.createStudent(data);
+    console.log(createdStudent);
+    return createdStudent;
+  }
 
-@Put(':id')
+  @Put(':id')
   async update(
     @Param('id') id: string,
     @Body() updatedPerson: Student,
   ): Promise<Student> {
-    return this.StudentService.update(id, updatedPerson);
+    return this.studentService.update(id, updatedPerson); // Fix the case here
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {
-    return this.StudentService.delete(id);
+    return this.studentService.delete(id); // Fix the case here
   }
 }
-
-
-
